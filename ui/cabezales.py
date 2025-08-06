e2aa71f2f3be546826aa7c70e4005c7701da7a84
martin federico sanchez <mfsanchez@inti.gob.ar>
2025-08-06 18:51:07 -0300

    agregué metodos a la clase cabezal

--- ui/cabezales.py
+++ ui/cabezales.py
@@ -18,6 +18,18 @@ class Cabezal:
     def chequeo_condicion_limite(self): #cada norma tiene sus límites esto es solo un warning
         print("aún en desarrollo")
         
+    def setear_corrosion_interna(self, corr_interna):
+        self.corr_interna = corr_interna
+    
+    def setear_corrosion_externa(self, corr_externa):
+        self.corr_externa = corr_externa
+        
+    def setear_radio_filete(self, radio_filete):
+        self.radio_filete = radio_filete
+
+    def setear_radio_corona(self, radio_corona):
+        self.radio_corona = radio_corona
+        
     def setear_tipo(self, tipo_cabezal):
         self.tipo = tipo_cabezal
         
@@ -26,6 +38,21 @@ class Cabezal:
         
     def setear_tipo_conformado(self, tipo_conformado):
         self.conformado = tipo_conformado
+        
+    def setear_altura_cabezal(self, altura):
+        self.altura = altura
+        
+    def setear_S(self, S):
+        self.S = S
+        
+    def setear_E(self, E):
+        self.E = E
+        
+    def setear_tipo_cabezal(self, tipo_cabezal):
+        self.tipo = tipo_cabezal
+        
+    def setar_tipo_conformado(self, conformado):
+        self.conformado = conformado
         
     def adicional_factor_conformado(self):
         if self.conformado == "En caliente":
@@ -41,7 +68,7 @@ class Cabezal:
         if self.tipo == "Semielíptico 2 : 1":
             self.espesor = (self.presion * self.diametro) / (2 * self.S * self.E)
             
-        elif self.tipo = "Semiesférico":
+        elif self.tipo == "Semiesférico":
             self.espesor = (self.presion * self.diametro) / (4 * self.S * self.E)
             
         elif self.tipo == "Toriesférico":
